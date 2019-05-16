73f4a083ad9169e01ddcf8b23dcbd834fc87e240
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-05-17 05:21:50 +0900

    Setup repository

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# types
+@bemybaby/types
+==
+
+TypeScript type definition enhancer
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+// Definitions by: Acid Chicken (硫酸鶏) <https://github.com/acid-chicken>
+// Definitions: https://github.com/bemybaby/types
+// TypeScript Version: 3.4.5
+
+export {}
